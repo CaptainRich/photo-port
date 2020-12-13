5c4abf7d6851caa5201b1eba6563176c20074089
RichA <rich_ay@sbcglobal.net>
2020-12-12 22:20:19 -0600

    Created 'controlled' contact form.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 
 
@@ -39,6 +40,7 @@ function App() {
       </Nav>     
 
       <main>
+          <ContactForm></ContactForm>
           <Gallery currentCategory={currentCategory}></Gallery>
           <About />        
       </main>
